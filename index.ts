77924216c8fa52ac1cc95072fdc0d8db77b1e7a3
Tiago Dias Provenzano <31420080+tiagodprovenzano@users.noreply.github.com>
2021-02-02 23:06:54 -0300

    removed class declaration

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-export { Connection } from "./src/Connection";
+import { createConnection } from "./src/Connection";
+
+const connection = createConnection('https://app9-api2.ploomes.com/')
+
+connection.login('teste@mrdias.com', '1234').then((uk) => console.log('login ok', uk))
--- src/Connection/index.ts
+++ src/Connection/index.ts
@@ -1,106 +1,104 @@
 import axios from 'axios';
 import { ISelf } from '@ploomes/ploomeststypes';
+import { IConnection } from './Types/IConnection';
 
-export class Connection {
-    
-    static userKey: string;
-    static baseUrl: string;
-    
-    constructor(baseUrl: string) {
-        Connection.baseUrl = baseUrl
-    }
 
-    login = (email: string, password: string): Promise<string> => {
-        return new Promise( (res, rej) => {
-            axios.post(
-                Connection.baseUrl + 'Self/Login?', { Email: email, Password: password }, {headers:  {'Content-Type': 'application/json'}}
-            )
-            .then(
-                value => {
-                    if(value.data){
-                        console.log(value.data)
-                        if(value.data?.value?.[0]){
-                            const userSelf: ISelf = value.data?.value?.[0] as ISelf
-                            Connection.userKey = userSelf.UserKey
-                        }else{
-                            rej(value.data.value)
+export const createConnection = (baseUrl: string): IConnection => {
+    const connection: IConnection = {
+        userKey: '',
+        baseUrl,
+        login : (email: string, password: string): Promise<string> => {
+            return new Promise( (res, rej) => {
+                axios.post(
+                    connection.baseUrl + 'Self/Login?', { Email: email, Password: password }, {headers:  {'Content-Type': 'application/json'}}
+                )
+                .then(
+                    value => {
+                        if(value.data){
+                            console.log(value.data)
+                            if(value.data?.value?.[0]){
+                                const userSelf: ISelf = value.data?.value?.[0] as ISelf
+                                connection.userKey = userSelf.UserKey
+                            }else{
+                                rej(value.data.value)
+                            }
+                            res(connection.userKey)
                         }
-                        res(Connection.userKey)
                     }
-                }
-            )
-            .catch(
-                error => {
-                    throw new Error(error);
-                }
-            )
-        })
-    }
-
-    get = (url) => {
-        return new Promise( (res, rej) => {
-            this.request(url, 'GET' )
-            .then(
-                data => {
-                    console.log(data)
-                    res(data)
-                }
-            )
-        })
-    }
-
-    post = (url, body) => {
-        return new Promise( (res, rej) => {
-            this.request(url, 'POST', body )
-            .then(
-                data => {
-                    console.log(data)
-                    res(data)
-                }
-            )
-        })
-    }
-
-    patch = (url, body) => {
-        return new Promise( (res, rej) => {
-            this.request(url, 'PATCH', body )
-            .then(
-                data => {
-                    console.log(data)
-                    res(data)
-                }
-            )
-        })
-    }
-
-    delete = (url) => {
-        return new Promise( (res, rej) => {
-            this.request(url, 'DELETE' )
-            .then(
-                data => {
-                    console.log(data)
-                    res(data)
-                }
-            )
-        })
-    }
-
-    request = (url: string,  method: 'GET'| 'POST' | 'DELETE' | 'PATCH', body: Record<string, any> = {}) => {
-        return new Promise( (res, rej) => {
-            axios.request(
-                {
-                    method, url: Connection.baseUrl + url, 
-                    headers: {'Content-Type': 'application/json', 'User-Key': Connection.userKey}, data: body
-                }
-            )
-            .then(
-                response => {
-                    if(response.data){
-                        res(response.data.value)
+                )
+                .catch(
+                    error => {
+                        throw new Error(error);
                     }
-                }
-            )
-            .catch( error => {throw new Error(error)})
-        })
+                )
+            })
+        },
+
+        get : (url: string):Promise<any> => {
+            return new Promise( (res, rej) => {
+                connection.request(url, 'GET' )
+                .then(
+                    data => {
+                        res(data)
+                    }
+                )
+            })
+        },
+
+        post : (url:string, body:any) => {
+            return new Promise( (res, rej) => {
+                connection.request(url, 'POST', body )
+                .then(
+                    data => {
+                        console.log(data)
+                        res(data)
+                    }
+                )
+            })
+        },
+
+        patch : (url: string, body: any) => {
+            return new Promise( (res, rej) => {
+                connection.request(url, 'PATCH', body )
+                .then(
+                    data => {
+                        console.log(data)
+                        res(data)
+                    }
+                )
+            })
+        },
+        
+        delete : (url: string) => {
+            return new Promise( (res, rej) => {
+                connection.request(url, 'DELETE' )
+                .then(
+                    data => {
+                        console.log(data)
+                        res(data)
+                    }
+                )
+            })
+        },
+
+        request : (url: string,  method: 'GET'| 'POST' | 'DELETE' | 'PATCH', body: Record<string, any> = {}) => {
+            return new Promise( (res, rej) => {
+                axios.request(
+                    {
+                        method, url: connection.baseUrl + url, 
+                        headers: {'Content-Type': 'application/json', 'User-Key': connection.userKey}, data: body
+                    }
+                )
+                .then(
+                    response => {
+                        if(response.data){
+                            res(response.data.value)
+                        }
+                    }
+                )
+                .catch( error => {throw new Error(error)})
+            })
+        }
     }
+    return connection
 }
