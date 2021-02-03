a299241ee80d863c6e90c6c3c613a27e14772e0e
Tiago Dias Provenzano <31420080+tiagodprovenzano@users.noreply.github.com>
2021-02-02 23:09:27 -0300

    fixed index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-import { createConnection } from "./src/Connection";
-
-const connection = createConnection('https://app9-api2.ploomes.com/')
-
-connection.login('teste@mrdias.com', '1234').then((uk) => console.log('login ok', uk))
+export { createConnection } from "./src/Connection";
