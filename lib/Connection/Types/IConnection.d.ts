a02f0f3c3b73e804c177e788042bd8c9e4ce4949
Tiago Dias Provenzano <31420080+tiagodprovenzano@users.noreply.github.com>
2021-02-03 01:12:08 -0300

    changing entry point

---
+++ lib/Connection/Types/IConnection.d.ts
@@ -0,0 +1,10 @@
+export interface IConnection {
+    userKey: string;
+    baseUrl: string;
+    login: (email: string, password: string) => Promise<string>;
+    get: (url: string) => Promise<any>;
+    post: (url: string, body: any) => Promise<any>;
+    patch: (url: string, body: any) => Promise<any>;
+    delete: (url: string) => Promise<any>;
+    request: (url: string, method: 'GET' | 'POST' | 'DELETE' | 'PATCH', body: Record<string, any>) => Promise<any>;
+}
---
+++ lib/Connection/createConnection.d.ts
@@ -0,0 +1,2 @@
+import { IConnection } from './Types/IConnection';
+export declare const createConnection: (baseUrl: string) => IConnection;
