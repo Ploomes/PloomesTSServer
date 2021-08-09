2e19c623a8e41649ff2ed416bc4e1aee50839c5e
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-08-09 15:18:16 -0300

    built new publish

--- lib/Connection/PloomesServer/index.d.ts
+++ lib/Connection/PloomesServer/index.d.ts
@@ -18,24 +18,24 @@ export declare class PloomesServer {
      * Sends a GET request to specified url.
      * returns Promise<any[]>
      */
-    get(url: string): Promise<any[]>;
+    get(url: string, headers?: Record<string, any>): Promise<any[]>;
     /**
      * delete
      * Deletes the Id specified at the url.
      * returns Promise<any[]>
      */
-    delete(url: string, body?: Record<string, any>): Promise<any[]>;
+    delete(url: string, body?: Record<string, any>, headers?: Record<string, any>): Promise<any[]>;
     /**
      * post
      * Sends a POST request to any Ploomes endpoint with provided body.
      * returns Promise<any[]>
      */
-    post(url: string, body: Record<string, any>): Promise<any[]>;
+    post(url: string, body: Record<string, any>, headers?: Record<string, any>): Promise<any[]>;
     /**
      * patch
      * Sends a PATCH request to any ploomes endpoint with provided body, updating item with the Id provided at the url.
      * returns Promise<any[]>
      */
-    patch(url: string, body: Record<string, any>): Promise<any[]>;
+    patch(url: string, body: Record<string, any>, headers?: Record<string, any>): Promise<any[]>;
     private request;
 }
